769d0aea02e8bb26668b9c77533db265c1164c5d
AndreaLi <andrea.li8341@hotmail.com>
2020-12-17 19:49:35 -0800

    nav bar basic structure

--- src/components/MainNav.jsx
+++ src/components/MainNav.jsx
@@ -1,37 +1,28 @@
 import React, { Component } from 'react';
 import { Route, Switch, Redirect, Link } from 'react-router-dom';
+import { MenuItems } from './MenuItems';
 
-export default class MainNav extends Component {
-  render() {
-    return (
-      <div className="navContainer">
-        <Link to="/" className="navbar" id="nav1">
-          Home
-        </Link>
-        <ul>
-          <li>Chatrooms</li>
-          <li>
-            <Link to="/en" className="navbar" id="nav2">
-              English
-            </Link>
-          </li>
-          <li>
-            <Link to="/fr" className="navbar" id="nav3">
-              French
-            </Link>
-          </li>
-          <li>
-            <Link to="/es" className="navbar" id="nav4">
-              Spanish
-            </Link>
-          </li>
-          <li>
-            <Link to="/de" className="navbar" id="nav5">
-              German
-            </Link>
-          </li>
-        </ul>
-      </div>
-    );
-  }
+export default function MainNav() {
+  return (
+    <nav className='MainNav'>
+      <img
+        className='logo'
+        src='https://icon-library.com/images/chatroom-icon/chatroom-icon-24.jpg'
+        height='100px'
+        width='100px'
+        alt='chatroom-logo'
+      />
+      <ul>
+        {MenuItems.map((item, idx) => {
+          return (
+            <li className='MainNav-item' key={`menuitem ${idx}`}>
+              <Link to={item.link} style={{ textDecoration: 'none' }}>
+                {item.itemName}
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
+    </nav>
+  );
 }
